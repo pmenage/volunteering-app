4415a2b5aacdd770bc8307e99651fceff9c5d8b9
Pauline <pauline.menage26@gmail.com>
2017-02-05 16:13:30 +0000

    See form values in console

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 angular
     .module('app', ['ngMessages'])
-    .controller('MainCtrl', MainCtrl);
+    .controller('MainCtrl', ['$scope', function($scope) {
+        $scope.submit = function() {
+            console.log("Submit");
+            if ($scope.userForm.$valid) {
+                console.log("First name: ", $scope.main.firstName);
+                console.log("Last name: ", $scope.main.lastName);
+                console.log("Email: ", $scope.main.email);
+                console.log("Gender: ", $scope.main.gender);
+                console.log("Skills: ", $scope.main.skills);
+                console.log("Time of day available: ", $scope.main.time);
+                console.log("Additional information: ", $scope.main.addInfo);
+            }
+        };
+    }]);
+
+//    .controller('MainCtrl', MainCtrl);
     
-function MainCtrl() {}
+// function MainCtrl() {}
